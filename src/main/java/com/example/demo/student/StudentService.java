b01091e06563b9e553609ef5b4b54cfbb8cd9895
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-09-24 10:19:18 -0500

    added DELETE and PUT methods

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,6 +1,8 @@
 package com.example.demo.student;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -23,4 +25,32 @@ public class StudentService {
         studentRepository.save(student);
         System.out.println(student);
     }
+
+    public void deleteStudent(Long studentId){
+        boolean exists=studentRepository.existsById(studentId);
+        if(!exists){
+            throw new IllegalStateException("student with id "+studentId+" does not exist");
+        }else{
+            studentRepository.deleteById(studentId);
+        }
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId,String name,String email){
+        Student student= studentRepository.findById(studentId).orElseThrow(()-> new IllegalStateException(
+                "student with id "+studentId+" does not exist"
+        ));
+        if(name!=null && name.length()>0 && !Objects.equals(student.getName(),name)){
+            student.setName(name);
+        }
+        if(email!=null && email.length()>0 && !Objects.equals(student.getEmail(),email)){
+            Optional<Student> studentOptional=studentRepository.findStudentsByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
+
+    }
+
 }
