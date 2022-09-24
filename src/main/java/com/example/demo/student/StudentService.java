6e8f0dcb2988972d4551ac85d103f06b4e31e075
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-09-23 23:42:59 -0500

    created a post method to connect with the postgresql database

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,21 +1,26 @@
 package com.example.demo.student;
-
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(2000,1,5),
-                        21
-                )
-        );
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student){
+        Optional<Student> studentOptional = studentRepository.findStudentsByEmail(student.getEmail());
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+        System.out.println(student);
     }
 }
